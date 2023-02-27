8d06c8c78bf397fae67ccf4ee977f35e42fb1ec9
noaboaaa <noarafati@hotmail.com>
2023-02-27 12:41:31 +0100

    v

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener("load",start);
+function start () {
+
+document.querySelector("#mushroom1").addEventListener("click",pauseMushroom1)
+
+function pausedMushroom1(){
+    document.querySelector("#mushroom1").classList.add("pause");
+}
+
+    // sakl tilføjes noget der stopper den efter jeg har clicket på den
+}
+
+
+//document.querySelector("mushroom1").addEventListener("click", pausedMushroom1);
