67f796f913b6803a001877522a5de480e79c765c
noaboaaa <noarafati@hotmail.com>
2023-03-04 02:11:38 +0100

    dåh

--- script.js
+++ script.js
@@ -1,36 +1,60 @@
-window.addEventListener("load",start);
+"use strict";
+window.addEventListener("load", ready);
 
+//globale variabler
 let points = 0;
 let lives = 0;
 
-function start(){
+function ready() {
+    console.log("JavaScript ready!");
+    document.querySelector("#btn_start").addEventListener("click", startGame);
+    document.querySelector("#try_again").addEventListener("click", startGame);
+    document.querySelector("#try_again1").addEventListener("click", startGame);
+}
 
-// start animations 
-document.querySelector("#mushroom1_container").classList.add("flyingm1");
-document.querySelector("#mushroom2_container").classList.add("flyingm2");
-document.querySelector("#mushroom4_container").classList.add("flyingm4");
-document.querySelector("#book2_container").classList.add("flyingb2");
-document.querySelector("#book3_container").classList.add("flyingb3");
+function startGame() {
+// point og liv nulstilles 
+  points = 0;
+  lives = 3;
+  //document.querySelector("#start_lyd").play();
+  //document.querySelector("#background_lyd").play();
 
-// click register 
+// startskærm skjules 
+document.querySelector("#start").classList.add("hidden");
+document.querySelector("#game_over").classList.add("hidden");
+document.querySelector("#level_complete").classList.add("hidden");
 
+// start alle animationer
+document.querySelector("#mushroom1_container").classList.add("flying");
+document.querySelector("#mushroom2_container").classList.add("flying");
+document.querySelector("#mushroom3_container").classList.add("flying");
+document.querySelector("#book1_container").classList.add("flying");
+document.querySelector("#book2_container").classList.add("flying");
+
+// indsæt poitions klasser
+document.querySelector("#mushroom1_container").classList.add("position1");
+document.querySelector("#mushroom2_container").classList.add("position2");
+document.querySelector("#mushroom3_container").classList.add("position3");
+document.querySelector("#book1_container").classList.add("position4");
+document.querySelector("#book2_container").classList.add("position5");
+
+// Registrer click 
 document.querySelector("#mushroom1_container").addEventListener("click", clickMushroom1);
 document.querySelector("#mushroom2_container").addEventListener("click", clickMushroom2);
-document.querySelector("#mushroom4_container").addEventListener("click", clickMushroom4);
+document.querySelector("#mushroom3_container").addEventListener("click", clickMushroom3);
+document.querySelector("#book1_container").addEventListener("click", clickBook1);
 document.querySelector("#book2_container").addEventListener("click", clickBook2);
-document.querySelector("#book3_container").addEventListener("click", clickBook3);
+}
 
 
-
-// bruger nu function for at : gentagne click,stop container, forsvind animation og efterfulgt af forsvind animation: containerSlut
 
 function clickMushroom1(){
 
     document.querySelector("#mushroom1_container").removeEventListener("click",clickMushroom1)
     document.querySelector("#mushroom1_container").classList.add("paused");
     document.querySelector("#mushroom1").classList.add("zoom_out");
-    document.querySelector("#mushroom1_container").addEventListener("animationend", mushroom1Gone);
-    decrementPoints();
+    document.querySelector("#mushroom1_container").addEventListener("animationend", Mushroom1Gone);
+    incrementPoints();
     displayPoints();
 }
 
@@ -39,19 +63,31 @@ function clickMushroom2(){
     document.querySelector("#mushroom2_container").removeEventListener("click",clickMushroom2)
     document.querySelector("#mushroom2_container").classList.add("paused");
     document.querySelector("#mushroom2").classList.add("zoom_out");
-    document.querySelector("#mushroom2_container").addEventListener("animationend", mushroom2Gone);
-    decrementPoints();
+    document.querySelector("#mushroom2_container").addEventListener("animationend", Mushroom2Gone);
+    incrementPoints();
     displayPoints();
 
 }
 
 
-function clickMushroom4(){
+function clickMushroom3(){
 
-    document.querySelector("#mushroom4_container").removeEventListener("click",clickMushroom4)
-    document.querySelector("#mushroom4_container").classList.add("paused");
-    document.querySelector("#mushroom4").classList.add("zoom_out");
-    document.querySelector("#mushroom4_container").addEventListener("animationend", mushroom4Gone);
+    document.querySelector("#mushroom3_container").removeEventListener("click",clickMushroom3)
+    document.querySelector("#mushroom3_container").classList.add("paused");
+    document.querySelector("#mushroom3").classList.add("zoom_out");
+    document.querySelector("#mushroom3_container").addEventListener("animationend", Mushroom3Gone);
+    incrementPoints();
+    displayPoints();
+}
+
+// clickBook
+
+function clickBook1(){
+
+    document.querySelector("#book1_container").removeEventListener("click", clickBook1)
+    document.querySelector("#book1_container").classList.add("paused");
+    document.querySelector("#book1").classList.add("zoom_in");
+    document.querySelector("#book1_container").addEventListener("animationend", Book1Gone);
     decrementPoints();
     displayPoints();
 }
@@ -61,17 +97,7 @@ function clickBook2(){
     document.querySelector("#book2_container").removeEventListener("click",clickBook2)
     document.querySelector("#book2_container").classList.add("paused");
     document.querySelector("#book2").classList.add("zoom_in");
-    document.querySelector("#book2_container").addEventListener("animationend", book2Gone);
-    decrementPoints();
-    displayPoints();
-}
-
-function clickBook3(){
-
-    document.querySelector("#book3_container").removeEventListener("click",clickBook3)
-    document.querySelector("#book3_container").classList.add("paused");
-    document.querySelector("#book3").classList.add("zoom_in");
-    document.querySelector("#book3_container").addEventListener("animationend", book3Gone);
+    document.querySelector("#book2_container").addEventListener("animationend", Book2Gone);
     decrementPoints();
     displayPoints();
 }
@@ -83,98 +109,129 @@ function Mushroom1Gone() {
   document.querySelector("#mushroom1_container").removeEventListener("animationend", Mushroom1Gone);
   document.querySelector("#mushroom1_container").classList.remove("zoom_out");
   document.querySelector("#mushroom1_container").classList.remove("paused");
-  document.querySelector("#mushroom1_container").classList.remove("falling");
+  document.querySelector("#mushroom1_container").classList.remove("flying");
   document.querySelector("#mushroom1_container").offsetWidth;
-  document.querySelector("#mushroom1_container").classList.add("falling");
+  document.querySelector("#mushroom1_container").classList.add("flying");
   document.querySelector("#mushroom1_container").addEventListener("click", clickMushroom1); 
+
 }
 function Mushroom2Gone() {
   document.querySelector("#mushroom2_container").removeEventListener("animationend", Mushroom2Gone);
   document.querySelector("#mushroom2_container").classList.remove("zoom_out");
   document.querySelector("#mushroom2_container").classList.remove("paused");
-  document.querySelector("#mushroom2_container").classList.remove("falling");
+  document.querySelector("#mushroom2_container").classList.remove("flying");
   document.querySelector("#mushroom2_container").offsetWidth;
-  document.querySelector("#mushroom2_container").classList.add("falling");
-  document.querySelector("#mushroom2_container").addEventListener("click", clickMushroom2); 
+  document.querySelector("#mushroom2_container").classList.add("flying");
+  document.querySelector("#mushroom2_container").addEventListener("click", clickMushroom2);
+ 
+ 
 }
 
-function Mushroom4Gone() {
-  document.querySelector("#mushroom4_container").removeEventListener("animationend", Mushroom4Gone);
-  document.querySelector("#mushroom4_container").classList.remove("zoom_out");
-  document.querySelector("#mushroom4_container").classList.remove("paused");
-  document.querySelector("#mushroom4_container").classList.remove("falling");
-  document.querySelector("#mushroom4_container").offsetWidth;
-  document.querySelector("#mushroom4_container").classList.add("falling");
-  document.querySelector("#mushroom4_container").addEventListener("click", clickMushroom4); 
+function Mushroom3Gone() {
+  document.querySelector("#mushroom3_container").removeEventListener("animationend", Mushroom3Gone);
+  document.querySelector("#mushroom3_container").classList.remove("zoom_out");
+  document.querySelector("#mushroom3_container").classList.remove("paused");
+  document.querySelector("#mushroom3_container").classList.remove("flying");
+  document.querySelector("#mushroom3_container").offsetWidth;
+  document.querySelector("#mushroom3_container").classList.add("flying");
+  document.querySelector("#mushroom3_container").addEventListener("click", clickMushroom3); 
+  
 }
 
 
 //dårlig ting: bookGone -point
 
+function Book1Gone() {
+  document.querySelector("#book1_container").removeEventListener("animationend", Book1Gone);
+  document.querySelector("#book1_container").classList.remove("zoom_in");
+  document.querySelector("#book1_container").classList.remove("paused");
+  document.querySelector("#book1_container").classList.remove("flying");
+  document.querySelector("#book1_container").offsetWidth;
+  document.querySelector("#book1_container").classList.add("flying");
+  document.querySelector("#book1_container").addEventListener("click", clickBook1); 
+
+}
+
 function Book2Gone() {
   document.querySelector("#book2_container").removeEventListener("animationend", Book2Gone);
   document.querySelector("#book2_container").classList.remove("zoom_in");
   document.querySelector("#book2_container").classList.remove("paused");
-  document.querySelector("#book2_container").classList.remove("falling");
+  document.querySelector("#book2_container").classList.remove("flying");
   document.querySelector("#book2_container").offsetWidth;
-  document.querySelector("#book2_container").classList.add("falling");
+  document.querySelector("#book2_container").classList.add("flying");
   document.querySelector("#book2_container").addEventListener("click", clickBook2); 
 }
 
-function Book3Gone() {
-  document.querySelector("#book3_container").removeEventListener("animationend", Book3Gone);
-  document.querySelector("#book3_container").classList.remove("zoom_in");
-  document.querySelector("#book3_container").classList.remove("paused");
-  document.querySelector("#book3_container").classList.remove("falling");
-  document.querySelector("#book3_container").offsetWidth;
-  document.querySelector("#book3_container").classList.add("falling");
-  document.querySelector("#book3_container").addEventListener("click", clickBook3); 
-}
 
+function clickBook1() {
+  document.querySelector("#book1_container").removeEventListener("click", clickBook1);
+  document.querySelector("#book1_container").classList.add("paused");
+  document.querySelector("#book1_container").classList.add("zoom_in");
+  document.querySelector("#book1_container").addEventListener("animationend", Book1Gone);
+}
 
 function clickBook2() {
   document.querySelector("#book2_container").removeEventListener("click", clickBook2);
   document.querySelector("#book2_container").classList.add("paused");
   document.querySelector("#book2_container").classList.add("zoom_in");
   document.querySelector("#book2_container").addEventListener("animationend", Book2Gone);
-  incrementPoints();
-  displayPoints();
+
 }
 
-function clickBook3() {
-  document.querySelector("#book3_container").removeEventListener("click", clickBook3);
-  document.querySelector("#book3_container").classList.add("paused");
-  document.querySelector("#book3_container").classList.add("zoom_in");
-  document.querySelector("#book3_container").addEventListener("animationend", Book3Gone);
-  incrementPoints();
-  displayPoints();
+// point liv osv
+function decrementLives() {
+  console.log("mist et liv");
+
+  if (lives <= 1) {
+    gameOver();
+  }
+
+  showDecrementedLives();
+  lives--;
+}
+function showDecrementedLives() {
+  document.querySelector("#heart" + lives).classList.remove("active_heart");
+  document.querySelector("#heart" + lives).classList.remove("active_heart");
 }
 
 
-
-
-
-
-
-
-// point ui 
-//Funktioner vi laver som øvelse i timen
-function incrementPoints() {
-  points += 1;
-  console.log(points);
+function incrementPoints(){
+  console.log("Giv point");
+  points++;
+  console.log("har nu" + point + "point");
   displayPoints();
-  if (points >= 10) {
+
+  if (points >= 10){
     levelComplete();
   }
 }
-function displayPoints() {
-  console.log("displayPoints");
-  document.querySelector("#point_count").textContent =points;
-}
-function decrementPoints() {
-  points -= 1;
-  console.log(points);
-  displayPoints();
+
+function displayPoints(){
+  console.log("vis point");
+  document.querySelector("#point_count").textContent = points;
 }
 
-} 
+
+function incrementLives(){
+  console.log("få et liv");
+  lives++;
+  showIncrementedLives();
+}
+
+
+function showIncrementedLives(){
+  document.querySelector("#heart" + lives).classList.remove("broken_heart");
+  document.querySelector("#heart" + lives).classList.remove("active_heart");
+
+}
+function gameOver() {
+  console.log("Game Over");
+  document.querySelector("#game_over").classList.remove("hidden");
+  stopGame();
+}
+
+function levelComplete(){
+  console.log("Level Complete");
+  document.querySelector("#level_complete").classList.remove("hidden");
+}
+
